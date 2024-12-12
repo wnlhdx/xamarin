94fdf90bb5e1f3f85705fbbf3c8ebbb85823d7eb
wnlhdx <314460007@qq.com>
2024-12-12 10:44:02 +0800

    MAUI

---
+++ start.sh
@@ -0,0 +1,1 @@
+cd MyMauiApp; dotnet build  -f net9.0-android "-p:AndroidSdkDirectory=/data/data/com.termux/files/home/Android/Sdk"
--- testapp.UI/Views/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xamarin.Forms;
-
-namespace testapp.Views;
-
-public sealed partial class MainPage : ContentPage
-{
-    public MainPage() =>
-        InitializeComponent();
-}
